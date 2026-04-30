61d95029617d469d40208ca9ef7e8935550f126c
SurajBhari <surajbhari159@gmail.com>
2026-04-30 07:31:35 +0530

    update useragent

--- scrapetube/scrapetube.py
+++ scrapetube/scrapetube.py
@@ -283,7 +283,7 @@ def get_session(proxies: dict = None, cookies: str = None) -> requests.Session:
         session.cookies.update(jar)
     session.headers[
         "User-Agent"
-    ] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    ] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36 Edg/147.0.0.0"
     session.headers["Accept-Language"] = "en"
     return session
 
